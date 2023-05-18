69c6985e32c0919f6232fafafa42f9290a464711
web2ls <web2ls@yandex.ru>
2023-05-18 23:21:53 +0400

    added posibility for setting interval value

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-const startReloadBtn = document.getElementById("start-reload-btn");
-const stopReloadBtn = document.getElementById("stop-reload-btn");
+const $startReloadBtn = document.getElementById("start-reload-btn");
+const $stopReloadBtn = document.getElementById("stop-reload-btn");
+const $timeInput = document.getElementById('time-input');
 let intervalId = null;
 
 const TIME = 3000;
+const SEC_IN_MIN = 60;
+const MS_IN_SEC = 1000;
 
-startReloadBtn.addEventListener("click",() => {    
+// TODO: Добавить таймер обратного отсчета между перезагрузками
+
+$startReloadBtn.addEventListener("click",() => {
         chrome.tabs.query({active: true}, (tabs) => {
         const tab = tabs[0];
         if (tab) {
-            startReloadBtn.setAttribute('disabled', true);
+            console.log($timeInput.value);
+            if (!$timeInput.value.trim().length) {
+                alert('Не указан интервал');
+                return false;
+            };
+
+            const timeValue = Number($timeInput.value);
+            if (isNaN(timeValue)) {
+                alert('Интервал указано некорректно');
+                return false;
+            };
+
+            const timeValueInMs = timeValue * SEC_IN_MIN * MS_IN_SEC;
+
+            $startReloadBtn.setAttribute('disabled', true);
             intervalId = setInterval(() => {
                 chrome.tabs.reload();
-            }, TIME);
+            }, timeValueInMs);
         } else {
             alert("There are no active tabs")
         }
@@ -19,8 +38,8 @@ startReloadBtn.addEventListener("click",() => {
 });
 
 
-stopReloadBtn.addEventListener('click', () => {
-    startReloadBtn.setAttribute('disabled', false);
+$stopReloadBtn.addEventListener('click', () => {
+    $startReloadBtn.setAttribute('disabled', false);
     clearInterval(intervalId);
-    window.close();    
+    window.close();
 });
