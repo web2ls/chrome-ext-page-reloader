005bc3090cac6469e7243158dca1a767593cebb7
web2ls <web2ls@yandex.ru>
2023-05-20 19:19:10 +0400

    added countdown timer

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const $startReloadBtn = document.getElementById("start-reload-btn");
 const $stopReloadBtn = document.getElementById("stop-reload-btn");
 const $timeInput = document.getElementById('time-input');
-let intervalId = null;
+const $countdown = document.getElementById('countdown');
+let timerIntervalId = null;
+let countdownIntervalId = null;
 
 const TIME = 3000;
 const SEC_IN_MIN = 60;
 const MS_IN_SEC = 1000;
-
-// TODO: Добавить таймер обратного отсчета между перезагрузками
 
 $startReloadBtn.addEventListener("click",() => {
         chrome.tabs.query({active: true}, (tabs) => {
@@ -26,10 +26,22 @@ $startReloadBtn.addEventListener("click",() => {
             };
 
             const timeValueInMs = timeValue * SEC_IN_MIN * MS_IN_SEC;
+            let time = timeValueInMs;
 
             $startReloadBtn.setAttribute('disabled', true);
-            intervalId = setInterval(() => {
+
+            countdownIntervalId = setInterval(() => {
+                time -= 1000;
+                const inSec = time / 1000;
+                const sec = (inSec % 60).toString().padStart(2, '0');
+                const min = Math.floor(inSec / 60).toString().padStart(2, '0');
+                const result = `${min} : ${sec}`;
+                $countdown.innerText = result;
+            }, 1000);
+
+            timerIntervalId = setInterval(() => {
                 chrome.tabs.reload();
+                time = timeValueInMs;
             }, timeValueInMs);
         } else {
             alert("There are no active tabs")
@@ -40,6 +52,7 @@ $startReloadBtn.addEventListener("click",() => {
 
 $stopReloadBtn.addEventListener('click', () => {
     $startReloadBtn.setAttribute('disabled', false);
-    clearInterval(intervalId);
+    clearInterval(timerIntervalId);
+    clearInterval(countdownIntervalId);
     window.close();
 });
